529347edf5933262144d0e9a14c39efe4782a644
AbhyasKanaujia <k.abhyas@gmail.com>
2021-10-26 20:04:53 +0530

    complete 1-6

--- 05 Compound Interest.c
+++ 05 Compound Interest.c
@@ -1,10 +1,18 @@
-#include < stdio.h>
+#include <math.h>
+#include <stdio.h>
 
 int main() {
+    float p, r, t;
+
     printf("Enter values:\n");
     printf("\tPrinciple: ");
     scanf("%f", &p);
     printf("\tRate: ");
     scanf("%f", &r);
-    printf("")
+    printf("\tTime: ");
+    scanf("%f", &t);
+
+    float ci = p * pow((1 + r / 100), t) - p;
+
+    printf("Compound Interest: %.2f", ci);
 }
