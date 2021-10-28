f9cb1bf03f66df3fcba52575996d3514e2e00c15
AbhyasKanaujia <k.abhyas@gmail.com>
2021-10-28 08:39:18 +0530

    make program 2 on arithmetic simpler

--- 02 Add Sub Mult Div.c
+++ 02 Add Sub Mult Div.c
@@ -1,52 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    double n1, n2;
+    int a, b;
     printf("Enter first number: ");
-    scanf("%lf", &n1);
+    scanf("%d", &a);
     printf("Enter second number: ");
-    scanf("%lf", &n2);
+    scanf("%d", &b);
 
-    while (1) {
-        int choice;
-        printf("\n\nMENU\n");
-        printf("1. Add\n");
-        printf("2. Subtract\n");
-        printf("3. Multiply\n");
-        printf("4. Divide\n");
-        printf("5. Change number\n");
-        printf("6. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                printf("\n%.2lf + %.2lf = %.2lf\n", n1, n2, n1 + n2);
-                break;
-            case 2:
-                printf("\n%.2lf - %.2lf = %.2lf\n", n1, n2, n1 - n2);
-                break;
-            case 3:
-                printf("\n%.2lf x %.2lf = %.2lf\n", n1, n2, n1 * n2);
-                break;
-            case 4:
-                if (n2 != 0)
-                    printf("\n%.2lf / %.2lf = %.2lf", n1, n2, n1 / n2);
-                else
-                    printf("\nCannot divide by 0\n");
-                break;
-            case 5:
-                printf("\nEnter first number: ");
-                scanf("%lf", &n1);
-                printf("Enter second number: ");
-                scanf("%lf", &n2);
-                break;
-            case 6:
-                return 0;
-            default:
-                printf("\nINVALID INPUT!\n");
-        }
-    }
+    printf("Sum: %d\n", a + b);
+    printf("Difference: %d\n", a - b);
+    printf("Product: %d\n", a * b);
+    if (b != 0)
+        printf("Quotient: %.2f", (float)a / b);
+    else
+        printf("Cannot divide by 0.");
 
     return 0;
 }
